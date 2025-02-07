7ab0ea07780f86f87df42ab2134d48bd71a32a45
Claudio Goncalves <claudio.goncalves@transitarconsultoria.com>
2025-02-07 15:17:24 -0300

    feat: added material light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+
 import { AppRoutes } from './routes';
+import { LightTheme } from './shared/themes';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
